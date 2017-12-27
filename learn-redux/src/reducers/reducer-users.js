8abed66b78c8c0009b20cce308e1af2e73d03e1b
Nithya Sam <nithyasam@gmail.com>
2017-12-26 19:35:36 -0600

    redux store creation

---
+++ learn-redux/src/reducers/reducer-users.js
@@ -0,0 +1,16 @@
+export default function(){
+    return [
+        {
+            id: 1,
+            first: "Nithya",
+            last: "Sam",
+            description: "Full stack developer"
+        },
+        {
+            id: 2,
+            first: "Abc",
+            last: "Xyz",
+            description: "Web developer"
+        }
+    ]
+}
