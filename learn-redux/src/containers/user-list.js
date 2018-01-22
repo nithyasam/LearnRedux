e9379419cf00d13d1200cc18165491d14e90fc3a
Nithya Sam <nithyasam@gmail.com>
2018-01-21 23:46:29 -0600

    created actions and creators

--- learn-redux/src/containers/user-list.js
+++ learn-redux/src/containers/user-list.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {selectUser} from '../actions/index';
 
 class UserList extends Component {
     createUserList(){
         return this.props.users.map(user => {
-                return(<li key = {user.id}>{user.first} {user.last}</li>);
+                return(<li key = {user.id}
+                        onClick = {() => this.props.selectUser(user)}>
+                        {user.first} {user.last}
+                        </li>);
             });
     }
     render(){
@@ -22,4 +26,9 @@ function mapStateToProps(state){
         users: state.users
     }
 }
-export default connect(mapStateToProps)(UserList);
+
+function matchDispatchToProps(dispatch) {
+    return bindActionCreators({selectUser : selectUser}, dispatch)
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
